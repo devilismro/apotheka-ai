1579536061f3919b6616140ca32ae9378a558655
devilismro <iulian.pampu1984@gmail.com>
2024-07-21 14:01:08 +0300

    no message

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,6 +1,9 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
-require('dotenv').config();
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
