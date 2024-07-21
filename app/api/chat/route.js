54f86b7c75bd19ba3ed8411a563fdbc2529f29c1
devilismro <iulian.pampu1984@gmail.com>
2024-07-21 18:20:32 +0300

    gpt4o

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -28,7 +28,7 @@ export async function POST(req) {
     const { messages } = await req.json();
 
     const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4o",
       stream: true,
       messages: [
         {
